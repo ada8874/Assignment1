9e7ccede79bc581823b421ddbacfcbd83bdcd87e
bububububububuu <ada_hashemi@hotmail.com>
2026-09-23 23:28:36 +0200

    notdone

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Assignment1
+
 Smart Fitness Session Analyzer
+
+Ada Hashemi, 8874
+
+Class design and Responsibility
+
+     Observation - One sensor measurement
+     Participant - Represents the person whose workout we are analyzing
+     WorkoutSession - the actual workout session
+     Anaylysisresult - What we concluded about the workout
+
+Running Instructions:
+
+1. git clone https://github.com/ada8874/REPOSITORY.git
+2. cd REPOSITORY
+3. python3 main.py
---
+++ sample_data.py
@@ -0,0 +1,26 @@
+observation_data = [
+    {
+        "timestamp": 1,
+        "heart_rate": 80,
+        "skin_response": 1.8,
+        "temperature": 32.5,
+        "activity_level": 0.20,
+        "signal_quality": 0.95
+    },
+    {
+        "timestamp": 2,
+        "heart_rate": 118,
+        "skin_response": 2.5,
+        "temperature": 32.9,
+        "activity_level": 0.72,
+        "signal_quality": 0.93
+    },
+    {
+        "timestamp": 3,
+        "heart_rate": 130,
+        "skin_response": 2.8,
+        "temperature": 33.1,
+        "activity_level": 0.85,
+        "signal_quality": 0.96
+    }
+]
